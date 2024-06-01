9fd1802c79303342409f5025fffb8366eec177ca
unknown <abdul7to7@gmail.com>
2024-06-02 02:11:46 +0530

    first server created

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Abdul Khalid");
+});
+
+server.listen(4000, () => {
+  console.log("Server running at http://127.0.0.1:4000/");
+});
