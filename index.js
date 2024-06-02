4d3c10bef4652076feb8bdf3a5377d8b75eedf1a
unknown <abdul7to7@gmail.com>
2024-06-03 04:34:44 +0530

    Post request and file system

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   if (req.url == "/home" || req.url == "/") {
-    res.end("Welcome Home");
-  } else if (req.url == "/about") {
-    res.end("Welcome to About Us page");
-  } else if (req.url == "/node") {
-    res.end("Welcome to my Node Js project");
+    res.write("<html>");
+    res.write("<head><title>Enter Data</title></head>");
+    res.write("<body>");
+    res.write(`<p>${fs.readFileSync("./messages.txt").toString()}</p>`);
+    res.write(
+      '<form action="/message" method="post"><input type="text" name="message"><button>send</button></form>'
+    );
+    res.write("</body>");
+    res.end();
+  } else if (req.url == "/message" && req.method == "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("messages.txt", message);
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
   } else {
     res.end("page not found");
   }
