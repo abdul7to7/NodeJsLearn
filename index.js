a68b0d068bdc253afcd75e525465ac6380315be3
unknown <abdul7to7@gmail.com>
2024-06-03 03:35:09 +0530

    Learn about request and response

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Abdul Khalid");
+  if (req.url == "/home" || req.url == "/") {
+    res.end("Welcome Home");
+  } else if (req.url == "/about") {
+    res.end("Welcome to About Us page");
+  } else if (req.url == "/node") {
+    res.end("Welcome to my Node Js project");
+  } else {
+    res.end("page not found");
+  }
 });
 
 server.listen(4000, () => {
