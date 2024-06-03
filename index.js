9bd503b82477d665b377aa4cde2e76510a532008
unknown <abdul7to7@gmail.com>
2024-06-03 17:57:29 +0530

    express js

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-const http = require("http");
 const handler = require("./routes");
 
-const server = http.createServer(handler);
+const express = require("express");
 
-server.listen(4000, () => {
-  console.log("Server running at http://127.0.0.1:4000/");
+const app = express();
+
+// Handle favicon request
+// app.get("/favicon.ico", (req, res) => res.status(204).end());
+
+app.use((req, res, next) => {
+  console.log("in the middleware");
+  console.log(req.url);
+  next();
 });
+
+app.use((req, res, next) => {
+  console.log("in another middleware");
+  res.send("<h1>Hello World</h1>");
+});
+
+app.listen(4000);
