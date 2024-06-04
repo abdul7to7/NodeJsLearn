a6af57acfec7fc673b6334359cc737746133cd47
unknown <abdul7to7@gmail.com>
2024-06-05 03:56:13 +0530

    post/get with express and body parser

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const handler = require("./routes");
 
 const express = require("express");
 
+const bodyParser = require("body-parser");
+
 const app = express();
 
 // Handle favicon request
 // app.get("/favicon.ico", (req, res) => res.status(204).end());
 
-app.use((req, res, next) => {
-  console.log("in the middleware");
-  console.log(req.url);
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/add-product", (req, res, next) => {
+  res.send(
+    "<body><form action='/products' method='POST'><input type='text' name='title'/><input type='text' name='size'/><button type='submit'>Submit</button></form></body>"
+  );
+});
+
+app.post("/products", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use((req, res, next) => {
-  console.log("in another middleware");
-  res.send("<h1>Hello World</h1>");
+  res.send("<h1>Home Page</h1>");
 });
 
 app.listen(4000);
